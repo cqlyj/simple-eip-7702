569672bd4cdfba74a0534c1bd1749df18ad32b3b
cqlyj <luoyingjie0721@gmail.com>
2025-04-24 00:47:31 +0800

    remove MockUSDC dependency from DeployDelegation script and update Delegation deployment

--- script/DeployDelegation.s.sol
+++ script/DeployDelegation.s.sol
@@ -3,18 +3,12 @@ pragma solidity 0.8.28;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Delegation} from "src/Delegation.sol";
-import {Vm} from "forge-std/Vm.sol";
 
 contract DeployDelegation is Script {
     function run() external {
-        address mockUSDC = Vm(address(vm)).getDeployment(
-            "MockUSDC",
-            uint64(block.chainid)
-        );
-
         vm.startBroadcast();
 
-        Delegation delegation = new Delegation(mockUSDC);
+        Delegation delegation = new Delegation();
         console.log("Delegation deployed at:", address(delegation));
         vm.stopBroadcast();
     }
