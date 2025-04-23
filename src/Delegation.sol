f59da39249cd054da5d75986e79e496cb8f2a390
cqlyj <luoyingjie0721@gmail.com>
2025-04-24 00:47:22 +0800

    remove MockUSDC contract and update Delegation contract to use native transfers

--- src/Delegation.sol
+++ src/Delegation.sol
@@ -1,30 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import {MockUSDC} from "src/MockUSDC.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 contract Delegation {
     mapping(address walletAddress => uint256 pin) public s_authorizedPins;
-    address public s_mockUSDC;
 
     error InvalidPin();
-
-    constructor(address mockUSDC) {
-        s_mockUSDC = mockUSDC;
-    }
+    event Authorized(address walletAddress, uint256 pin);
+    event Pay(address sender, address to, uint256 pin);
 
     // Authorize a pin to be used for moving funds
     function authorize(address walletAddress, uint256 pin) external {
         s_authorizedPins[walletAddress] = pin;
+        emit Authorized(walletAddress, pin);
     }
 
     // Pay once get the pin
-    function pay(address payable to, uint256 amount, uint256 pin) external {
-        if (s_authorizedPins[msg.sender] == pin) {
-            IERC20(s_mockUSDC).transfer(to, amount);
+    function payFrom(
+        address from,
+        address payable to,
+        uint256 amount,
+        uint256 pin
+    ) external {
+        if (s_authorizedPins[from] == pin) {
+            (bool success, ) = to.call{value: amount}("");
+            emit Pay(from, to, pin);
         } else {
             revert InvalidPin();
         }
     }
+
+    receive() external payable {}
 }
